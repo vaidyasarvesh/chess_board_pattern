b08a01ae03b848c1c164e20db48940d0b09809ee
Sarvesh Vaidya <132005888+vaidyasarvesh@users.noreply.github.com>
2023-07-24 21:57:20 +0530

    Add files via upload

---
+++ chessboard/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chessboard
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int size = 10; // Size of the chessboard (8x8);
+            int row;
+            int col;
+
+            for ( row = 0; row < size; row++)
+            {
+                for ( col = 0; col < size; col++)
+                {
+                    if ((row + col) % 2 == 0)
+                    {
+                        Console.Write("██"); // Black square
+                    }
+                    else
+                    {
+                        Console.Write("  "); // White square
+                    }
+                }
+
+                Console.WriteLine(); // Move to the next line for the next row
+            }
+
+            Console.ReadLine(); // Wait for user input to close the console window
+        }
+    }
+}
